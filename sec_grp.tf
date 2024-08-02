29e0ee6afecc517baf379ba6bd0c2a2aa7677dfe
budhishmadana <aws.budhish@gmail.com>
2024-08-02 20:13:35 +0530

    adding 8080 port in sec groups

--- sec_grp.tf
+++ sec_grp.tf
@@ -17,6 +17,12 @@ resource "aws_security_group" "sec_group" {
   }
 
   ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["${var.vpc_cidr}"]
+  }
+  ingress {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
